7752f2aaf16284cb5159787b0219065d2da3caaa
selinal46 <66965580+selinal46@users.noreply.github.com>
2020-06-15 14:56:22 -0400

    Add files via upload

---
+++ Python-class1-hello.py
@@ -0,0 +1,13 @@
+print("hello world")
+
+name = input("best country in the world: ")
+print(name)
+
+name = name.lower()
+if (name == "canada"):
+    print(name + " is the best!")
+else:
+    print("boo wrong answer")
+
+fruits = ['watermelon','apple','pear']
+print(fruits)
